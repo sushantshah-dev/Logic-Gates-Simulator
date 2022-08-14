cc43f2e0302932bddaf4bdd1fbd03a77e5f72f2c
Sushant Shah <sushantshah@cyrocoders.dev>
2022-08-14 07:56:42 +0530

    Create Basic Load/Unload Script With Empty Main

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys, pickle
+
+def main(baseObject=None):
+    pass
+
+def save(baseObject, filename):
+    pickle.dump(baseObject, open(filename, 'wb'))
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main(pickle.load(open(sys.argv[1], 'rb')))
+    elif len(sys.argv) == 1:
+        main()
